fb43ab1d3ba640164b76e35ad2705e0d7e353313
gshashikant <gshashikant@riverbed.com>
2019-07-30 10:37:25 +0530

    Basic binary search

---
+++ searching/binary_search_basic.h
@@ -0,0 +1,20 @@
+#ifndef BINARY_SEARCH_BASIC_H_INCLUDED
+#define BINARY_SEARCH_BASIC_H_INCLUDED
+
+using namespace std;
+
+int binary_basic_search(vector<int> &arr, int low, int high, int ele){
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]==ele){
+            return mid;
+        }else if(arr[mid]<ele){
+            low=mid-1;
+        }else{
+            high=mid+1;
+        }
+    }
+    return -1;
+}
+
+#endif // BINARY_SEARCH_BASIC_H_INCLUDED
