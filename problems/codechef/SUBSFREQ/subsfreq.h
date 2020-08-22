d30bb35a6ae15033569361086c621b2d1ed399b7
Gaurav Shashikant <sgau0522@gmail.com>
2020-08-22 19:17:20 +0530

    Subsfreq fail.

--- problems/codechef/SUBSFREQ/subsfreq.h
+++ problems/codechef/SUBSFREQ/subsfreq.h
@@ -8,6 +8,72 @@
 
 using namespace std;
 
+void initialize(vector<vector<int> > &mem, int n){
+    for(int i=0; i<n; i++){
+        mem[i][0] = 1;
+        mem[i][n-1] = 1;
+    }
+    return;
+}
+
+void print(vector<vector<int> > &mem, int n){
+    for(int i=n-1; i>=0; i--){
+        for(int j=0; j<n; j++){
+            cout<<mem[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return;
+}
+
+void solve(vector<int> &input, int n, vector<vector<int> > &mem){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(input[j]>=i){
+                if(input[j-1]>=i){
+                    mem[i][j] = mem[i][j-1] * i;
+                }else{
+                    mem[i][j] = mem[i][j-1] * (input[j-1]+1);
+                }
+            }else{
+                mem[i][j] = mem[i][j-1];
+            }
+        }
+    }
+    print(mem, n+2);
+    for(int i=1; i<=n; i++){
+        for(int j=n; j>=1; j--){
+            if(input[j]>=i){
+                //mem[i][j]--;
+                if(input[j+1]>=i){
+                    mem[i][j] *= (mem[i][j+1] * (i+1));
+                }else{
+                    mem[i][j] *= (mem[i][j+1] * (input[j+1]+1));
+                }
+            }else{
+                mem[i][j] = mem[i][j+1];
+            }
+        }
+    }
+
+    int temp=0;
+    for(int j=1; j<=n; j++){
+        if(input[j]!=0){
+            for(int i=1; i<=input[j]; i++){
+                temp = (temp + mem[i][j]) % 1000000007;
+            }
+            cout<<temp<<" ";
+            temp=0;
+        }else{
+            cout<<"0"<<" ";
+        }
+    }
+    cout<<endl;
+    return;
+}
+
+
+
 int subsfreq(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -17,16 +83,17 @@ int subsfreq(){
     cin>>t;
 
     while(t--){
-        int n;
+        int n, temp;
         cin>>n;
-        vector<int> input(n);
+        vector<int> input(n+2);
+        vector<vector<int> > mem(n+2, vector<int>(n+2));
         for(int i=0; i<n; i++){
-            cin>>input[i];
+            cin>>temp;
+            input[temp]++;
         }
-
-        std::sort(input.begin(), input.end());
-
-        cout<<""<<endl;
+        initialize(mem, n+2);
+        solve(input, n, mem);
+        print(mem, n+2);
     }
 
     return 0;
