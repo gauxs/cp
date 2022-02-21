b9a2da7e600eeb183a594ed4fd36c0a56e31fa52
gauxs <sgau0522@gmail.com>
2022-02-21 20:24:07 +0530

    Bipartite-II

--- algorithms/graph/bipartite/README.md
+++ algorithms/graph/bipartite/README.md
@@ -1,7 +1,7 @@
 # Bipartite
 A bipartite graph is a graph whose vertices can be divided into two disjoint sets so that every edge connects two vertices from different sets (i.e. there are no edges which connect vertices from the same set). These sets are usually called **sides**. You are given an undirected graph. Check whether it is bipartite, and if it is, output its sides.
 
-There exists a theorem which claims that a graph is bipartite if and only if all its cycles have even length. However, in practice it's more convenient to use a different formulation of the definition: **a graph is bipartite if and only if it is two-colorable**.
+There exists a theorem which claims that a **graph is bipartite if and only if all its cycles have even length**. However, in practice it's more convenient to use a different formulation of the definition: **a graph is bipartite if and only if it is two-colorable**.
 
 ## Algorithm Analysis
 ```
--- algorithms/graph/bipartite/bipartite.h
+++ algorithms/graph/bipartite/bipartite.h
@@ -1,51 +1,36 @@
-#ifndef BFS_H_INCLUDED
-#define BFS_H_INCLUDED
+#ifndef BIPARTITE_H_INCLUDED
+#define BIPARTITE_H_INCLUDED
 
 #include <vector>
 #include <queue>
 #include <bits/stdc++.h>
 
-void bfs(
+bool is_bipartite(
     std::vector<std::vector<int>> &adj,
-        std::vector<int> &parents, 
-            std::vector<int> &distance,
-                std::vector<bool> &picked,
-                    int n, int source){
+        std::vector<bool> &picked,
+            std::vector<char> &color,
+                int n, int source){
 
     std::queue<int> q;
     picked[source] = true;
-    parents[source] = -1;
+    color[source] = 1;
     q.push(source);
     while(!q.empty()){
         int cur_vertex = q.front();
         q.pop();
         for(int i=0; i<n; i++){
+            if(i!=cur_vertex && color[i]==color[cur_vertex])
+                return false;
+
             if(!picked[i] && adj[cur_vertex][i]!=0){
                 picked[i] = true;
-                parents[i] = cur_vertex;
-                distance[i] = distance[cur_vertex] + 1;
+                color[i] = color[cur_vertex]==1?2:1;
                 q.push(i);
             }
         }
     }
 
-    return;
+    return true;
 }
 
-std::vector<int> get_path(
-    std::vector<int> &parents, 
-        std::vector<bool> &picked, int v){
-    
-    std::vector<int> path;
-    if(picked[v]){
-        while(v!=-1){
-            path.push_back(v);
-            v = parents[v];
-        }
-    }
-
-    reverse(path.begin(), path.end());
-    return path;
-}
-
-#endif // BFS_H_INCLUDED
+#endif // BIPARTITE_H_INCLUDED
