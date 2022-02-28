f586e0b11d59c83d004c187a4f8cf84ec0ac8f58
gauxs <sgau0522@gmail.com>
2022-02-28 15:01:44 +0530

    Sorting-VIII.

---
+++ algorithms/sort/comparison/shell/shell.h
@@ -0,0 +1,27 @@
+#ifndef SHELL_H_INCLUDED
+#define SHELL_H_INCLUDED
+
+#include <vector>
+#include <iostream>
+#include "utils/basic.h"
+
+void shell_sort(std::vector<int> &input){
+    int n = input.size();
+
+    // gap goes from [n/2, 1]
+    for(int gap=n/2; gap>0; gap/=2){
+        for(int i=gap; i<n; i++){
+            int temp = input[i];
+            int j;
+            // j>=gap because we are checking input[j-gap]
+            for(j=i; j>=gap && input[j-gap]>temp; j-=gap){
+                input[j] = input[j-gap];
+            }
+            input[j] = temp;
+        }
+    }
+    
+    return;
+}
+
+#endif // SHELL_H_INCLUDED
--- algorithms/sort/comparison/tim_sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIM_SORT_H_INCLUDED
-#define TIM_SORT_H_INCLUDED
-
-/*
-    Reference:  https://www.geeksforgeeks.org/timsort/
-*/
-
-#endif // TIM_SORT_H_INCLUDED
